c94c31c47be5a3fdadcbf2b5d19e2bed672eeae3
Mateus <mhenrique4060@gmail.com>
2026-02-04 08:35:53 -0300

    testing migration

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,19 @@
+-- Números de linha
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Linha do cursor destacada
+vim.opt.cursorline = true
+
+-- Coluna guia (ex: 80 chars)
+vim.opt.colorcolumn = "100"
+
+-- Melhor leitura
+vim.opt.signcolumn = "yes"
+
+vim.opt.laststatus = 3
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
