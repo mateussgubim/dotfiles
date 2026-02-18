b696a7cc960f89691f8a9cae70d427d6808999ab
Mateus <mhenrique4060@gmail.com>
2026-02-18 20:15:47 -0300

    change color scheme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,86 +1,117 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-
-  config = function()
-    require("catppuccin").setup({
-      flavour = "mocha",
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-      transparent_background = false,
-      float = {
-        transparent = false,
-        solid = false,
-      },
-      show_end_of_buffer = false,
-      term_colors = false,
-      dim_inactive = {
-        enabled = false,
-        shade = "dark",
-        percentage = 0.15,
-      },
-      no_italic = false,
-      no_bold = false,
-      no_underline = false,
-
-      styles = {
-        comments = { "italic" },
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-      },
-
-      lsp_styles = {
-        virtual_text = {
-          errors = { "italic" },
-          hints = { "italic" },
-          warnings = { "italic" },
-          information = { "italic" },
-          ok = { "italic" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        background = {
+          light = "latte",
+          dark = "mocha",
         },
-        underlines = {
-          errors = { "underline" },
-          hints = { "underline" },
-          warnings = { "underline" },
-          information = { "underline" },
-          ok = { "underline" },
+        transparent_background = false,
+        float = {
+          transparent = false,
+          solid = false,
         },
-        inlay_hints = {
-          background = true,
+        show_end_of_buffer = false,
+        term_colors = false,
+        dim_inactive = {
+          enabled = false,
+          shade = "dark",
+          percentage = 0.15,
         },
-      },
-
-      color_overrides = {},
-      custom_highlights = {},
-
-      default_integrations = true,
-      auto_integrations = false,
-
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        neotree = true, -- ajuste correto
-        notify = false,
-        mini = {
-          enabled = true,
-          indentscope_color = "",
+        no_italic = false,
+        no_bold = false,
+        no_underline = false,
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
         },
-      },
-    })
+        lsp_styles = {
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+            ok = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+            ok = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        default_integrations = true,
+        auto_integrations = false,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          neotree = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+        },
+      })
+      -- vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 
-    -- ISSO É SEU CÓDIGO, só no lugar certo
-    vim.cmd.colorscheme("catppuccin")
-  end,
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        terminal_colors = true,
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        inverse = true,
+        contrast = "",
+        palette_overrides = {
+          bright_green = "#990000",
+        },
+        overrides = {
+          SignColumn = { bg = "#ff9900" },
+          ["@lsp.type.method"] = { bg = "#ff9900" },
+          ["@comment.lua"] = { bg = "#000000" },
+        },
+        dim_inactive = false,
+        transparent_mode = false,
+      })
+
+      vim.cmd("colorscheme gruvbox")
+    end,
+  },
 }
-
