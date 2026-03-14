dde5ac9f05cb1356bee867ba7128ef6e6b747ce7
Mateus <mhenrique4060@gmail.com>
2026-03-14 08:11:34 -0300

    add JS/TS formating tools

---
+++ nvim/lua/plugins/formatting.lua
@@ -0,0 +1,24 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" }, -- Run before saving
+  cmd = { "ConformInfo" },
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      json = { "prettierd" },
+      yaml = { "prettierd" },
+      markdown = { "prettierd" },
+      lua = { "stylua" },
+      python = { "isort", "black" },
+    },
+    -- This is where the magic happens:
+    format_on_save = {
+      lsp_fallback = true,
+      async = false,
+      timeout_ms = 500,
+    },
+  },
+}
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,24 +1,58 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
     config = function()
-      -- LSPs básicos
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "vtsls",
+          "eslint",
+          "lua_ls",
+          "terraformls",
+          "dockerls",
+          "bashls",
+          "pyright",
+          "yamlls",
+        },
+      })
+
       vim.lsp.config("lua_ls", {})
       vim.lsp.config("terraformls", {})
       vim.lsp.config("dockerls", {})
       vim.lsp.config("bashls", {})
       vim.lsp.config("pyright", {})
 
-      -- YAML com schemas DevOps
+      vim.lsp.config("vtsls", {
+        settings = {
+          typescript = {
+            inlayHints = {
+              parameterNames = { enabled = "all" },
+              variableTypes = { enabled = true },
+            },
+          },
+        },
+      })
+
+      vim.lsp.config("eslint", {
+        on_attach = function(client, bufnr)
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            command = "EslintFixAll",
+          })
+        end,
+      })
+
       vim.lsp.config("yamlls", {
         settings = {
           yaml = {
             schemas = {
               kubernetes = "*.yaml",
-              ["https://json.schemastore.org/github-workflow.json"] =
-                ".github/workflows/*",
-              ["https://json.schemastore.org/github-action.json"] =
-                ".github/action.{yml,yaml}",
+              ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*",
+              ["https://json.schemastore.org/github-action.json"] = ".github/action.{yml,yaml}",
             },
           },
         },
@@ -26,4 +60,3 @@ return {
     end,
   },
 }
-
